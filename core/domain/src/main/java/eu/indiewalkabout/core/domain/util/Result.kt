aa196e2b9b546c9211402cd22f892664210042ac
simone <simone.mapelli@gmail.com>
2026-01-14 19:01:24 +0100

    feat: implement string res management for error handling

--- core/domain/src/main/java/eu/indiewalkabout/core/domain/util/Result.kt
+++ core/domain/src/main/java/eu/indiewalkabout/core/domain/util/Result.kt
@@ -1,7 +1,5 @@
 package eu.indiewalkabout.core.domain.util
 
-
-
 sealed interface Result<out D, out E: Error> {
     data class Success<out D>(val data: D): Result<D, Nothing>
     data class Error<out E: eu.indiewalkabout.core.domain.util.Error>(val error: E): Result<Nothing, E>
