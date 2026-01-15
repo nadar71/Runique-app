0beb96b7fb7c96b02550c4133eac9fb504c26a2a
simone <simone.mapelli@gmail.com>
2026-01-15 20:55:22 +0100

    feat: first build & deploy ok

--- app/src/main/java/eu/indiewalkabout/runique/MainActivity.kt
+++ app/src/main/java/eu/indiewalkabout/runique/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import eu.indiewalkabout.runique.ui.theme.RuniqueTheme
+import eu.indiewalkabout.core.presentation.designsystem.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +22,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Greeting("Android")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RuniqueTheme {
+        Greeting("Android")
     }
 }
 
--- app/src/main/java/eu/indiewalkabout/runique/ui/theme/Color.kt
+++ app/src/main/java/eu/indiewalkabout/runique/ui/theme/Color.kt
@@ -2,14 +2,10 @@ package eu.indiewalkabout.runique.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val RuniqueGreen = Color(0xFF00F15E)
-val RuniqueGreen30 = Color(0x4D00F15E)
-val RuniqueGreen10 = Color(0x1A00F15E)
-val RuniqueGreen5 = Color(0x0D00F15E)
-val RuniqueBlack = Color(0xFF080707)
-val RuniqueGray = Color(0xFF87938C)
-val RuniqueGray40 = Color(0x6687938C)
-val RuniqueDarkGray = Color(0xFF232624)
-val RuniqueWhite = Color(0xFFFAFAFA)
-val RuniqueDarkRed = Color(0xFFBB3D3D)
-val RuniqueDarkRed5 = Color(0x0DBB3D3D)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
