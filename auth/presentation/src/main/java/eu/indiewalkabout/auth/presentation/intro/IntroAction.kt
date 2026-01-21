5b0ad49a64a1f6b524ba77a6c6ddeaafa20a83db
simone <simone.mapelli@gmail.com>
2026-01-21 16:15:50 +0100

    feat: 6.1-IntroScreenUI and components, onAction behaviour

---
+++ auth/presentation/src/main/java/eu/indiewalkabout/auth/presentation/intro/IntroAction.kt
@@ -0,0 +1,6 @@
+package eu.indiewalkabout.auth.presentation.intro
+
+sealed interface IntroAction {
+    data object OnSignInClick: IntroAction
+    data object OnSignUpClick: IntroAction
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,8 @@ dependencyResolutionManagement {
     }
 }
 
+gradle.startParameter.excludedTaskNames.addAll(listOf(":build-logic:convention:testClasses"))
+
 rootProject.name = "Runique"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS") // This allows i.e. implementation(projects.auth.domain) instead of hardcoded strings.
