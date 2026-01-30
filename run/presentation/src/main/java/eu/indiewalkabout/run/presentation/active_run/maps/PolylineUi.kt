5e297f9a5c631ede807ccf1a48089484b3cd8297
simone <simone.mapelli@gmail.com>
2026-01-30 18:23:19 +0100

    feat: 7.11 Drawing the running track on the map

---
+++ run/presentation/src/main/java/eu/indiewalkabout/run/presentation/active_run/maps/PolylineUi.kt
@@ -0,0 +1,10 @@
+package eu.indiewalkabout.run.presentation.active_run.maps
+
+import androidx.compose.ui.graphics.Color
+import eu.indiewalkabout.core.domain.location.Location
+
+data class PolylineUi(
+    val location1: Location,
+    val location2: Location,
+    val color: Color
+)
