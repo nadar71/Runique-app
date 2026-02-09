fae09c278143491d05e2efc08b01b02da49d0ca7
simone <simone.mapelli@gmail.com>
2026-02-09 19:05:30 +0100

    feat: 10.4 Calculating run analytics

---
+++ analytics/data/src/main/java/eu/indiewalkabout/analytics/data/RoomAnalyticsRepository.kt
@@ -0,0 +1,32 @@
+package eu.indiewalkabout.analytics.data
+
+import eu.indiewalkabout.analytics.domain.AnalyticsRepository
+import eu.indiewalkabout.analytics.domain.AnalyticsValues
+import eu.indiewalkabout.core.database.dao.AnalyticsDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import kotlin.time.Duration.Companion.milliseconds
+
+class RoomAnalyticsRepository(
+    private val analyticsDao: AnalyticsDao
+): AnalyticsRepository {
+
+    override suspend fun getAnalyticsValues(): AnalyticsValues {
+        return withContext(Dispatchers.IO) {
+            val totalDistance = async { analyticsDao.getTotalDistance() }
+            val totalTimeMillis = async { analyticsDao.getTotalTimeRun() }
+            val maxRunSpeed = async { analyticsDao.getMaxRunSpeed() }
+            val avgDistancePerRun = async { analyticsDao.getAvgDistancePerRun() }
+            val avgPacePerRun = async { analyticsDao.getAvgPacePerRun() }
+
+            AnalyticsValues(
+                totalDistanceRun = totalDistance.await(),
+                totalTimeRun = totalTimeMillis.await().milliseconds,
+                fastestEverRun = maxRunSpeed.await(),
+                avgDistancePerRun = avgDistancePerRun.await(),
+                avgPacePerRun = avgPacePerRun.await()
+            )
+        }
+    }
+}
---
+++ analytics/domain/src/main/java/eu/indiewalkabout/analytics/domain/AnalyticsRepository.kt
@@ -0,0 +1,5 @@
+package eu.indiewalkabout.analytics.domain
+
+interface AnalyticsRepository {
+    suspend fun getAnalyticsValues(): AnalyticsValues
+}
